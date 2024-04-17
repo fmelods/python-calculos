9ed8509cb8a4f0406e1e527f3be93427c8cdb070
fmelods <felipemelodesousa4@gmail.com>
2024-04-17 14:19:35 -0300

    4 exercícios feitos

---
+++ lacos.py
@@ -0,0 +1,25 @@
+# 1 - 1 a 100
+num = 0
+while num != 100:
+    num = num + 1
+    print(f"Número: {num}")
+
+print()
+
+# 2 - 50 a 100
+num = 49
+while num != 100:
+    num = num + 1
+    print(f"Número: {num}")
+
+print()
+
+# 3 - Contagem regressiva
+num = 11
+while num != 0:
+    num = num - 1
+    print(f"Número: {num}")
+
+print()
+
+# 4 - 
